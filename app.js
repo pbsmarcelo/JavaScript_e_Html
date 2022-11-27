84d41eb79625e09004068964d9b0a16bfc396e8e
Marcelo <pbsmarcelo@gmail.com>
2022-11-27 12:52:19 -0300

    Add if pontuacao

--- app.js
+++ app.js
@@ -13,6 +13,20 @@ var vitorias = parseInt(prompt("Entre com o número de vitórias."));
 var empates = parseInt(prompt("Entre com o número de empates."));
 
 var pontuacao = (vitorias * 3) + empates;
+
 mostra("Seu time possui até o momento: " + pontuacao + " pontos!");
 
+if (pontuacao > 28) {
+    mostra("Seu time esta melhor do que no ano passado.");
+}
 
+if (pontuacao < 28){
+    mostra("Seu time esta pior do que no ano passado.");
+}
+
+if (pontuacao == 28){
+    mos
+    mostra("Seu time esta igual ao ano passado.");
+}
+
+mostra("FIM")
