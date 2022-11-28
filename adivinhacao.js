1ff409807f11d43c119c2c6f75f36971a4b18727
Marcelo <pbsmarcelo@gmail.com>
2022-11-28 00:01:49 -0300

    Melhoria no jogo

--- adivinhacao.js
+++ adivinhacao.js
@@ -8,14 +8,25 @@ function mostra(frase) {
     pulaLinha();
 }
 
-var numeroPensado = Math.round(Math.random() * 10); 
+function sorteia (n){
+    return Math.round(Math.random() * n);
+}
+
+//var numeroPensado = Math.round(Math.random() * 10); usado antes da funcao
+
+var numeroPensado = sorteia(10);
+console.log(numeroPensado); // mostra dentro do inspecionar o numero
 
 var chute = parseInt(prompt("Digite o seu chute:"));
 
 if(chute == numeroPensado){
-    mostra("Voce acertou o chute! Parabéns!");
+    mostra("Voce acertou o chute! pois eu pensei justamente no número: " + numeroPensado);
 } else{
-    mostra("Infelizmente você errou! o número pensado foi: " + numeroPensado);
+    if(chute > numeroPensado){
+        mostra("Infelizmente você errou! seu chute foi maior do que o numero pensado.");
+    }else {
+        mostra("Infelizmente você errou! seu chute foi menor do que o numero pensado.");
+    }
 }
 
 
