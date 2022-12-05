16afbdd5565fc511beef438ae5ed91179314d2c9
Marcelo <pbsmarcelo@gmail.com>
2022-12-04 21:41:04 -0300

    Add for duplo montar linhas x colunas

--- adivinhacao.js
+++ adivinhacao.js
@@ -17,17 +17,20 @@ function sorteia (n){
 var numeroPensado = sorteia(10);
 console.log(numeroPensado); // mostra dentro do inspecionar o numero
 
-var chute = parseInt(prompt("Digite o seu chute:"));
+var tentativas = 1;
 
-if(chute == numeroPensado){
-    mostra("Voce acertou o chute! pois eu pensei justamente no número: " + numeroPensado);
-} else{
-    if(chute > numeroPensado){
-        mostra("Infelizmente você errou! seu chute foi maior do que o numero pensado.");
-    }else {
-        mostra("Infelizmente você errou! seu chute foi menor do que o numero pensado.");
+while(tentativas <= 3){
+
+    var chute = parseInt(prompt("Digite o seu chute:"));
+
+    if(chute == numeroPensado){
+        mostra("Voce acertou o chute!");
+       break;
+
+    } else{
+            mostra("Infelizmente você errou!");
     }
+    tentativas++;
 }
 
-
-
+mostra("FIM");
